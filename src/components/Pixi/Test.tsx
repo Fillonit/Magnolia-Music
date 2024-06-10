5d8fc1e055f2a0653d9453af802111c6e78c2228
Fillonit Ibishi <filloniti@gmail.com>
2024-06-10 20:33:11 +0200

    Deployment Fix

--- src/components/Pixi/Test.tsx
+++ src/components/Pixi/Test.tsx
@@ -1,48 +1,48 @@
-import { useEffect, useRef } from "react";
-import { Application, Loader, Sprite, Ticker } from "pixi.js";
+// import { useEffect, useRef } from "react";
+// import { Application, Loader, Sprite, Ticker } from "pixi.js";
 
-const PixiComponent = () => {
-	const pixiContainer = useRef<HTMLDivElement | null>(null);
+// const PixiComponent = () => {
+// 	const pixiContainer = useRef<HTMLDivElement | null>(null);
 
-	useEffect(() => {
-		const app = new Application({
-			width: 800,
-			height: 600,
-			backgroundColor: 0x1099bb,
-		});
+// 	useEffect(() => {
+// 		const app = new Application({
+// 			width: 800,
+// 			height: 600,
+// 			backgroundColor: 0x1099bb,
+// 		});
 
-		if (pixiContainer.current) {
-			pixiContainer.current.appendChild(app.view as unknown as Node);
-		}
+// 		if (pixiContainer.current) {
+// 			pixiContainer.current.appendChild(app.view as unknown as Node);
+// 		}
 
-		Loader.sharedLoader.add("bunny", "path/to/bunny.png").load(setup);
+// 		Loader.sharedLoader.add("bunny", "path/to/bunny.png").load(setup);
 
-		let bunny: Sprite;
+// 		let bunny: Sprite;
 
-		function setup() {
-			bunny = new Sprite(.sharedLoader.resources.bunny.texture);
+// 		function setup() {
+// 			bunny = new Sprite(.sharedLoader.resources.bunny.texture);
 
-			bunny.x = app.view.width / 2;
-			bunny.y = app.view.height / 2;
+// 			bunny.x = app.view.width / 2;
+// 			bunny.y = app.view.height / 2;
 
-			app.stage.addChild(bunny);
+// 			app.stage.addChild(bunny);
 
-			app.ticker.add((ticker: Ticker) => gameLoop(ticker.deltaTime));
-		}
+// 			app.ticker.add((ticker: Ticker) => gameLoop(ticker.deltaTime));
+// 		}
 
-		function gameLoop(deltaTime: number) {
-			bunny.rotation += 0.01 * deltaTime;
-		}
+// 		function gameLoop(deltaTime: number) {
+// 			bunny.rotation += 0.01 * deltaTime;
+// 		}
 
-		return () => {
-			app.destroy(true, {
-				children: true,
-				texture: true,
-			});
-		};
-	}, []);
+// 		return () => {
+// 			app.destroy(true, {
+// 				children: true,
+// 				texture: true,
+// 			});
+// 		};
+// 	}, []);
 
-	return <div ref={pixiContainer} />;
-};
+// 	return <div ref={pixiContainer} />;
+// };
 
-export default PixiComponent;
+// export default PixiComponent;
